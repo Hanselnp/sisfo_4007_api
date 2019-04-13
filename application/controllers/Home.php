ceaf899a8a3523671755c34519cb82470ea6a77a
hanselnp <www.hanselnp992gmail.com>
2019-04-13 10:59:50 +0700

    Fix bug update

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Home extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -18,8 +18,16 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('home_model');
+	}
+
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$data['banner'] = $this->home_model->getBanner();
+		$data['article'] = $this->home_model->getArticle();
+		$data['about'] = $this->home_model->getAbout();
+		$this->load->view('home', $data);
 	}
 }
--- application/controllers/Template.php
+++ application/controllers/Template.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Template extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -18,8 +18,12 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
+	public function index(){
+		$this->load->view('template');
 	}
+
+	public function it_support(){
+		$this->load->view('it_support');
+	}
+	
 }
